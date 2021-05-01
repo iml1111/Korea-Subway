f3dda3660d36f1d24cb4cd3df74978a9154a5714
IML <shin10256@gmail.com>
2021-05-01 20:59:42 +0900

    test

--- util.py
+++ util.py
@@ -1,16 +1,42 @@
 import csv, json
 from collections import defaultdict
+from nogada import capitalStationGraph
 
+def make():
+	file_path = './dataset/capital/station_coordinate.csv'
+	station_dict = defaultdict(
+		lambda: {
+			"lines": [], 
+			'latitude': None, 
+			'longitude': None,
+			'around_stations': [],
+		}
+	)
 
-file_path = './dataset/capital/station_coordinate.csv'
-station_dict = defaultdict(lambda: {"lines":[], 'lat': None, 'lng': None})
+	with open(file_path, 'r', encoding='utf-8') as f:
+		reader = list(csv.reader(f))[1:]
+		for item in reader:
+			line, name, _, lat, lng = item
+			station_dict[name]['lines'].append(line)
+			station_dict[name]['latitude'] = float(lat)
+			station_dict[name]['longitude'] = float(lng)
+			station_dict[name]['around_stations'] = capitalStationGraph[name]
 
-with open(file_path, 'r') as f:
-	reader = list(csv.reader(f))[1:]
-	for item in reader:
-		line, name, lat, lng = item
-		station_dict[name]['lines'].append(line)
-		station_dict[name]['latitude'] = lat
-		station_dict[name]['longitude'] = lng
+	docs = []
+	for name, value in station_dict.items():
+		doc = {
+			'name': name,
+			**value
+		}
+		docs.append(doc)
 
-		
+	with open("./dataset/capital/capital_stations.csv", "w") as f:
+		json.dump(docs, f, indent='\t')
+
+def load():
+	with open("./dataset/capital/capital_stations.csv", "r") as f:
+		a = json.load(f)
+		print(a)
+
+make()
+load()
